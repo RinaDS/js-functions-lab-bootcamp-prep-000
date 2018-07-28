24d04cb14a4fa5b11a079f7a0aca1af124046d5c
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-28 19:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function happyHolidaysTo (name){
 function happyHolidayTo (holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
+//function with two paramters and a calculation
+function holidayCountdown (days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
