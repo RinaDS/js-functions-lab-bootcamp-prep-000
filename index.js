a1933a65ae3f93be749fd0c3cd5d86a1b043a366
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-28 19:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // write your code below!
+//Happy holidays!
 function happyHolidays () {
   return "Happy holidays!"
 }
   
+//Happy holidays throw
+function happyHolidaysTo (name){
+  return `Happy holidays, ${name}!`
+}
